d8369a24aea358dee53c643389ebe9e86d94a372
Cooper Koo <cooper@cagacrypto.com>
2025-11-22 23:02:41 +0800

    fix: Suppress hydration warning on html element

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
+    <html suppressHydrationWarning lang="en">
       <body className={`${outfit.className} dark:bg-gray-900`}>
         <Web3Providers>
           <ThemeProvider>
